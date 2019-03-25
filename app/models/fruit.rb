52d05774f6c296a6651ed4de6ab0dd515eb9bd49
Jason Groff <jgroff88@gmail.com>
2019-03-25 13:26:26 -0500

    CRUD Operations (#1)
    * Add ReactJS library to asset pipeline
    
    * Added fruit model and corresponding migration / seeds
    
    * Updated routes
    
    * Added fruits_controller and CRUD actions
    
    * Added home or index view
    
    * Added main and all_fruits React components
    
    * Added validations for fruit model
    
    * Added form to add new fruit, added delete button to delete an item from the fruit list
    
    * Added update and delete functionality
    
    * Formatted with prettier

---
+++ app/models/fruit.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: fruits
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string(255)
+#  description :text(65535)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Fruit < ApplicationRecord
+  validates :id, presence: true, uniqueness: true
+  validates :name, :description, presence: true
+end
